9c104d26f9d3493fb5e3353d3d2811eec848e048
Drithh <adrielalfeus@gmail.com>
2022-03-29 12:08:43 +0700

    remove broken link

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# HOME PAGE P!NGFEST 2021 Website
+# HOME PAGE P!NGFEST 2021
 
 [Main Repository](https://docs.google.com/document/d/1BxKbHLX_YJN_rGFglfV9htlzLX-b8260m94InH-ZlAk/edit)
--- public/pingfest/js/semnas.js
+++ public/pingfest/js/semnas.js
@@ -45,83 +45,83 @@ jQuery(document).ready(function () {
   });
 });
 
-// class TextScramble {
-//   constructor(el) {
-//     this.el = el;
-//     this.chars = 'FindYourIdea';
-//     this.update = this.update.bind(this);
-//   }
-//   setText(newText) {
-//     const oldText = this.el.innerText;
-//     const length = Math.max(oldText.length, newText.length);
-//     const promise = new Promise((resolve) => (this.resolve = resolve));
-//     this.queue = [];
-//     for (let i = 0; i < length; i++) {
-//       const from = oldText[i] || '';
-//       const to = newText[i] || '';
-//       const start = Math.floor(Math.random() * 20);
-//       const end = start + Math.floor(Math.random() * 20);
-//       this.queue.push({ from, to, start, end });
-//     }
-//     cancelAnimationFrame(this.frameRequest);
-//     this.frame = 0;
-//     this.update();
-//     return promise;
-//   }
-//   update() {
-//     let output = '';
-//     let complete = 0;
-//     for (let i = 0, n = this.queue.length; i < n; i++) {
-//       let { from, to, start, end, char } = this.queue[i];
-//       if (this.frame >= end) {
-//         complete++;
-//         output += to;
-//       } else if (this.frame >= start) {
-//         if (!char || Math.random() < 0.28) {
-//           char = this.randomChar();
-//           this.queue[i].char = char;
-//         }
-//         output += `<span class="dud">${char}</span>`;
-//       } else {
-//         output += from;
-//       }
-//     }
-//     this.el.innerHTML = output;
-//     if (complete === this.queue.length) {
-//       this.resolve();
-//     } else {
-//       this.frameRequest = requestAnimationFrame(this.update);
-//       this.frame++;
-//     }
-//   }
-//   randomChar() {
-//     return this.chars[Math.floor(Math.random() * this.chars.length)];
-//   }
-// }
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = 'FindYourIdea';
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 20);
+      const end = start + Math.floor(Math.random() * 20);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = '';
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
 
-// var idea = document.querySelector('.idea');
-// var textArray = document.getElementsByClassName('hay-text');
+var idea = document.querySelector('.idea');
+var textArray = document.getElementsByClassName('hay-text');
 
-// var indexText = 1;
-// var hayText = ['Find', 'Your'];
-// var counter = 2;
-// $(document).on('mouseenter', '.idea', function () {
-//   if (counter == 0) {
-//     counter = 3;
-//   }
-//   counter--;
-//   const fxIdea = new TextScramble(idea);
-//   const fxText = new TextScramble(textArray[counter]);
+var indexText = 1;
+var hayText = ['Find', 'Your'];
+var counter = 2;
+$(document).on('mouseenter', '.idea', function () {
+  if (counter == 0) {
+    counter = 3;
+  }
+  counter--;
+  const fxIdea = new TextScramble(idea);
+  const fxText = new TextScramble(textArray[counter]);
 
-//   fxIdea.setText(hayText[indexText]);
-//   fxText.setText('Idea');
+  fxIdea.setText(hayText[indexText]);
+  fxText.setText('Idea');
 
-//   indexText = Math.abs(indexText - 1);
+  indexText = Math.abs(indexText - 1);
 
-//   idea.classList.remove('idea');
-//   textArray[counter].classList.add('idea');
-//   idea = document.querySelector('.idea');
-// });
+  idea.classList.remove('idea');
+  textArray[counter].classList.add('idea');
+  idea = document.querySelector('.idea');
+});
 
 var daftarText = document.querySelector('.daftar-text');
 var daftarBlock = document.querySelector('.daftar-block');
