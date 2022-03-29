c2cb2c94349f0187ceb9581c9454568a6c16b815
Drithh <adrielalfeus@gmail.com>
2022-03-29 11:53:18 +0700

    clean up files

--- application/helpers/rupiah_helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit;
-
-function rupiah($amount) {
-    return 'Rp. '.number_format($amount, 2, ',', '.');
-}
-
--- application/views/profile/fake_login.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php echo site_url('profile/fake_login'); ?>" method="post">
-    <div><input type="text" class="form-control" name="user_id" placeholder="User ID"></div>
-    <div><button type="submit" class="btn btn-success" name="submit" value="1">Login!</button></div>
-</form>
